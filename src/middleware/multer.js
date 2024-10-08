82283735110fe852ebe548bb8e477300c39ec2d1
shumail786-hash <sherrymines@outlook.com>
2024-10-08 17:37:31 +0500

    set: cloudinary storage for image upload

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,16 +1,25 @@
 import multer from "multer";
-import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import dotenv from "dotenv";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+dotenv.config({ path: ".env" });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads",
+    allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
 
 export const upload = multer({
   storage: storage,
-  limits: { fileSize: 1024 * 1024 * 1 }, // Limit file size to 1MB
+  // limits: { fileSize: 1024 * 1024 * 2 },  Limit file size to 2MB
 });
